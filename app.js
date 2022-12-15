d6ab1c7c67582a2439a7e6acfea8f55be92a29dc
nabilmaal <119407093+nabilmaal@users.noreply.github.com>
2022-12-15 21:53:45 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,16 @@
+/* ---------------NAVBAR-----------------*/
+
+const MENUBURGER = document.querySelector(".menu-burger")
+const navLinks = document.querySelector(".nav-links")
+
+MENUBURGER.addEventListener('click',()=>{
+navLinks.classList.toggle('mobile-menu')
+})
+
+/* ---------------MAIN-----------------*/
+
+
+
+
+
+
