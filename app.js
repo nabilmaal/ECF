c5e38da74a3c97a7c6c3dc2fd34a2fc2075cfd58
nabilmaal <119407093+nabilmaal@users.noreply.github.com>
2022-12-16 16:01:43 +0100

    version final

--- app.js
+++ app.js
@@ -11,6 +11,89 @@ navLinks.classList.toggle('mobile-menu')
 
 
 
+/*NAME*/
+
+fetch('https://www.themealdb.com/api/json/v1/1/random.php')
+.then(x => x.json())
+.then (y => {
+ let div = document.querySelector("#day p")
+ div.innerHTML = y.meals[0].strMeal
+})
+
+fetch('https://www.themealdb.com/api/json/v1/1/random.php')
+.then(x => x.json())
+.then (y => {
+ let div1 = document.querySelector("#day1 p")
+ div1.innerHTML = y.meals[0].strMeal
+})
+
+fetch('https://www.themealdb.com/api/json/v1/1/random.php')
+.then(x => x.json())
+.then (y => {
+ let div1 = document.querySelector("#day2 p")
+ div1.innerHTML = y.meals[0].strMeal
+})
+
+fetch('https://www.themealdb.com/api/json/v1/1/random.php')
+.then(x => x.json())
+.then (y => {
+ let div1 = document.querySelector("#day3 p")
+ div1.innerHTML = y.meals[0].strMeal
+
+
+})
+
+
+fetch('https://www.themealdb.com/api/json/v1/1/random.php')
+.then(x => x.json())
+.then (y => {
+  let div1 = document.querySelector("#day4 p")
+  div1.innerHTML = y.meals[0].strMeal
+})
+
+
+fetch('https://www.themealdb.com/api/json/v1/1/random.php')
+.then(x => x.json())
+.then (y => {
+ let div1 = document.querySelector("#etp p")
+ div1.innerHTML = y.meals[0].strInstructions
+})
+
+/*etape*/
+
+
+fetch('https://www.themealdb.com/api/json/v1/1/random.php')
+.then(x => x.json())
+.then (y => {
+ let div1 = document.querySelector("#etp1 p")
+ div1.innerHTML = y.meals[0].strInstructions
+})
+
+
+fetch('https://www.themealdb.com/api/json/v1/1/random.php')
+.then(x => x.json())
+.then (y => {
+ let div1 = document.querySelector("#etp2 p")
+ div1.innerHTML = y.meals[0].strInstructions
+})
+
+
+fetch('https://www.themealdb.com/api/json/v1/1/random.php')
+.then(x => x.json())
+.then (y => {
+ let div1 = document.querySelector("#etp3 p")
+ div1.innerHTML = y.meals[0].strInstructions
+})
+
+
+fetch('https://www.themealdb.com/api/json/v1/1/random.php')
+.then(x => x.json())
+.then (y => {
+ let div1 = document.querySelector("#etp4 p")
+ div1.innerHTML = y.meals[0].strInstructions
+})
+
+/*image*/ 
 
 
 
